f22ac10d1ec85365013508db3529b170971e1e79
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-06 14:57:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
-st.header('🎈 App Name')
+st.title('🎈 Different ways to display images')
 
-st.write('Hello world!')
+st.header('1. Using `st.image`')
+
+from PIL import Image
+img = Image.open('streamlit-logo-secondary-colormark-darktext.png')
+st.image(img)
+
+st.code('''from PIL import Image
+  img = Image.open('streamlit-logo-secondary-colormark-darktext.png')
+  st.image(img)
+''')
+
+st.header('2. Using `st.markdown`')
+
+st.markdown('<img src="streamlit-logo-secondary-colormark-darktext.png">', unsafe_allow_html=True)
+
+st.code('''st.markdown('<img src="streamlit-logo-secondary-colormark-darktext.png">', unsafe_allow_html=True)
+''')
