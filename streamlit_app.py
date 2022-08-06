957840d8920126cf3864ada581188ed338727c32
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-06 15:52:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ from PIL import Image
 img = Image.open('streamlit-logo-secondary-colormark-darktext.png')
 st.image(img)
 
+st.subheader('Code')
 st.code('''from PIL import Image
   img = Image.open('streamlit-logo-secondary-colormark-darktext.png')
   st.image(img)
@@ -37,3 +38,23 @@ def img_to_html(img_path):
 
 st.markdown(img_to_html('streamlit-logo-secondary-colormark-darktext.png'), unsafe_allow_html=True)
 
+st.subheader('Code')
+st.code('''# img_to_bytes and img_to_html inspired from https://pmbaumgartner.github.io/streamlitopedia/sizing-and-images.html
+import base64
+from pathlib import Path
+from utilities import load_bootstrap
+
+load_bootstrap()
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+def img_to_html(img_path):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+      img_to_bytes(img_path)
+    )
+    return img_html
+
+st.markdown(img_to_html('streamlit-logo-secondary-colormark-darktext.png'), unsafe_allow_html=True)
+''')
